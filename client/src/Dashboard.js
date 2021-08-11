63fcd7f969860213d81f02b85a9d8aa597b63522
Eduardo Melo <eduardoccmelo@gmail.com>
2021-08-11 15:30:52 +0200

    add style into search bar

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -6,13 +6,17 @@ export default function Dashboard({ code }) {
   const accessToken = useAuth(code);
   const [search, setSearch] = useState("");
   return (
-    <Container>
+    <Container className="d-flex flex-column py-2" style={{ height: "100vh" }}>
       <Form.Control
         type="search"
         placeholder="Search Songs/Artists"
         value={search}
         onChange={(e) => setSearch(e.target.value)}
-      ></Form.Control>
+      />
+      <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>
+        Songs
+      </div>
+      <div>Bottom</div>
     </Container>
   );
 }
