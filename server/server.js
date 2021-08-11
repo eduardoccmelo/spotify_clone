b386ba4be362560c35376b6dd99336b3ded6fa54
Eduardo Melo <eduardoccmelo@gmail.com>
2021-08-11 17:55:18 +0200

    create .env file to hold the tokens data

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
@@ -12,9 +13,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.post("/refresh", (req, res) => {
   const refreshToken = req.body.refreshToken;
   const spotifyApi = new SpotifyWebApi({
-    redirectUri: "http://localhost:3000",
-    clientId: "c33217b081124d1a9a7fbd229b05ab34",
-    clientSecret: "b408c682a8dd40d187944644e53949ef",
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
     refreshToken,
   });
 
@@ -35,9 +36,9 @@ app.post("/refresh", (req, res) => {
 app.post("/login", (req, res) => {
   const code = req.body.code;
   const spotifyApi = new SpotifyWebApi({
-    redirectUri: "http://localhost:3000",
-    clientId: "c33217b081124d1a9a7fbd229b05ab34",
-    clientSecret: "b408c682a8dd40d187944644e53949ef",
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
   });
 
   spotifyApi
